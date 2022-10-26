4b890aab57b2d85bbbe7d96f428030ad00ec5768
Branley Minaya <bminaya2@gmail.com>
2022-10-26 04:30:19 -0400

    firebase

---
+++ src/firbase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTsjoSNRYpPapZvAcZ-R088rSJkNML47I",
+  authDomain: "cryptobase-e9cc5.firebaseapp.com",
+  projectId: "cryptobase-e9cc5",
+  storageBucket: "cryptobase-e9cc5.appspot.com",
+  messagingSenderId: "358086003524",
+  appId: "1:358086003524:web:286a265fb73e2b95142511"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
